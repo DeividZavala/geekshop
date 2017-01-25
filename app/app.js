dcd4763856a9893daa6e4a99ba62fb178398490b
DeividZavala <crusedmoss@gmail.com>
2017-01-25 15:46:05 -0600

    landing component added :octocat:

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@
         .controller("tabsMenu",tabsMenu);
 
         function tabsMenu($scope,$location) {
-            console.log("entre al controller")
 
             $scope.selectedTab = 1;
             $scope.checkRoute = checkRoute;
