6263c090c470ab1b00055e25a5d9dfd4948de964
dianita8 <diana_kari@live.com.mx>
2017-02-14 13:15:36 -0600

    slider

--- app/components/catalogo/compoCatalog.js
+++ app/components/catalogo/compoCatalog.js
@@ -14,7 +14,7 @@
 
           $scope.openNav = openNav
           $scope.closeNav = closeNav
-          $scope.next = next
+
 
 
             function openNav() {
@@ -27,10 +27,37 @@
                  document.getElementById("main").style.marginLeft= "0";
             }
 
-            function next(){
-              document.getElementById("imgsCarrito").style.padding = "500px";
-            }
+              $(document).ready(function() {
+                    $('.carousel_ul li:first').before($('.carousel_ul li:last'));
 
+                    $('#right_scroll i').click(function(){
+
+                        var item_width = $('.carousel_ul li').outerWidth() + 10;
+
+                        var left_indent = parseInt($('.carousel_ul').css('left')) - item_width;
+
+                        $('.carousel_ul:not(:animated)').animate({'left' : left_indent},500,function(){
+
+                            $('.carousel_ul li:last').after($('.carousel_ul li:first'));
+
+                            $('.carousel_ul').css({'left' : '-255px'});
+                        });
+                    });
+
+                    $('#left_scroll i').click(function(){
+
+                        var item_width = $('.carousel_ul li').outerWidth() + 10;
+
+                        var left_indent = parseInt($('.carousel_ul').css('left')) + item_width;
+
+                        $('.carousel_ul:not(:animated)').animate({'left' : left_indent},500,function(){
+
+                        $('.carousel_ul li:first').before($('.carousel_ul li:last'));
+
+                        $('.carousel_ul').css({'left' : '-210px'});
+                        });
+                    });
+              });
 
   }
 
